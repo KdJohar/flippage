8b6cd6f4ce7e0ba3a82bdf494e4ff70f37ca77be
kdjohar <kdjohar91@gmail.com>
2016-04-03 01:21:24 +0530

    template added

--- flippage/flippage/local_settings.py
+++ flippage/flippage/local_settings.py
@@ -9,6 +9,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_ROOT = os.path.join(BASE_DIR, "../static")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "../media")
 
 DATABASES = {
     'default': {
--- flippage/flippage/urls.py
+++ flippage/flippage/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^/', 'books.views.home', name='home'),
+    url(r'^demo/$', 'books.views.bookdetail', name='book_detail'),
 ]
