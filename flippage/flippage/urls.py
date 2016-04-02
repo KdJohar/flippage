e8d91a790a92b9a55e9eba9406eb41448952e1d9
kdjohar <kdjohar91@gmail.com>
2016-04-03 01:31:54 +0530

    changes done

--- flippage/flippage/urls.py
+++ flippage/flippage/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^/', 'books.views.home', name='home'),
-    url(r'^demo/$', 'books.views.bookdetail', name='book_detail'),
+    url(r'^', 'books.views.home', name='demo'),
+    url(r'^demo/', 'books.views.bookdetail', name='demo2'),
 ]
