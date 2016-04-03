604ecdb5a51cb9c550f71773e9cbe2392aba4a1a
kdjohar <kdjohar91@gmail.com>
2016-04-03 12:48:44 +0530

    book app added

--- flippage/books/admin.py
+++ flippage/books/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
-
+from .models import Author, Publisher, Category, SubCategory, Books, Inventory, SeoMetaData
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'category', 'isbn10', 'isbn13', 'cost_price', 'selling_price', 'mrp')
+    list_filter = ('category', 'author', 'publisher', )
+    search_fields = ('title', )
+    list_editable = ('selling_price', )
+    # prepopulated_fields = {"slug": ("title",)}
+
+class SubCategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'category', )
+    list_filter = ('category', )
+    prepopulated_fields = {"slug": ("name",)}
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {"slug": ("name",)}
+
+
+class InventoryAdmin(admin.ModelAdmin):
+
+    list_display = ('book', 'quantity')
+    search_fields = ('book__title', )
+    list_editable = ('quantity', )
+
+class SeoMetaDataAdmin(admin.ModelAdmin):
+
+    list_display = ('book', 'seo_done', )
+    search_fields = ('book__title', )
+    list_filter = ('seo_done'),
+    list_editable = ('seo_done', )
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    prepopulated_fields = {"slug": ("name",)}
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    prepopulated_fields = {"slug": ("name",)}
+
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Books, BookAdmin)
+admin.site.register(Inventory, InventoryAdmin)
+admin.site.register(SeoMetaData, SeoMetaDataAdmin)
--- flippage/books/models.py
+++ flippage/books/models.py
@@ -1,5 +1,137 @@
 from __future__ import unicode_literals
-
+from django.core.validators import RegexValidator
 from django.db import models
+from django.db.models import signals
+from django.utils.text import slugify
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True, null=True)
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Author"
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Publisher"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    category = models.ForeignKey(Category)
+    slug = models.SlugField(max_length=250, unique=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "SubCategories"
+
+
+def image_upload_to(instance, filename):
+    title = slugify(instance.title)
+    file_extention = filename.split(".")[1]
+    file_name = "%s.%s"%(title, file_extention)
+    return "book/%s/%s"%(title, file_name)
+
+class Books(models.Model):
+
+    LANGUAGE_CHOICE = (
+        ('en', 'english'),
+    )
+
+    BINDING_CHOICE = (
+        ('pb', 'paperback'),
+    )
+
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    image = models.ImageField(upload_to=image_upload_to)
+    category = models.ForeignKey(SubCategory)
+    isbn10 = models.CharField(validators=[RegexValidator(regex='^.{10}$', message='Length has to be 10')], max_length=10)
+    isbn13 = models.CharField(validators=[RegexValidator(regex='^.{13}$', message='Length has to be 13')], max_length=13)
+    publisher = models.ForeignKey(Publisher)
+    author = models.ManyToManyField(Author)
+    language = models.CharField(max_length=2, choices=LANGUAGE_CHOICE)
+    binding = models.CharField(max_length=2, choices=BINDING_CHOICE)
+    cost_price = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    mrp = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+
+    def get_image(self):
+        return self.image.url
+
+
+    def save(self, *args, **kwargs):
+
+        self.slug = slugify(self.title)
+        if self.selling_price == None:
+            cost = int(self.cost_price)
+            percent_margin = 15
+            packing_cost = 5
+            cost = cost + (cost*percent_margin)/100
+            self.selling_price = cost+packing_cost
+        super(Books, self).save(*args, **kwargs)
+
+
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Books"
+
+
+class Inventory(models.Model):
+
+    book = models.OneToOneField(Books)
+    quantity = models.IntegerField(default=3)
+
+    def __unicode__(self):
+        return self.book.title
+    class Meta:
+        verbose_name_plural = "Inventory"
+
+class SeoMetaData(models.Model):
+
+    book = models.OneToOneField(Books)
+    title = models.CharField(max_length=250, blank=True, null=True)
+    keywords = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    seo_done = models.BooleanField(default=False)
+
+
+    def __unicode__(self):
+        return self.book.title
+    class Meta:
+        verbose_name_plural = "Book Page Metadata"
+
+def create_inventory_seo_for_book(sender, instance, created, **kwargs):
+    if created:
+        Inventory.objects.create(book=instance)
+        SeoMetaData.objects.create(book=instance)
+
+
+signals.post_save.connect(create_inventory_seo_for_book, sender=Books)
